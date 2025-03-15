41274e3b238c31bee072dedd03f5143ecd55355f
Suresh <suris6530@gmail.com>
2025-03-15 12:51:06 +0530

    content updated

--- src/components/Activitylab/ActivityLab.jsx
+++ src/components/Activitylab/ActivityLab.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Grid, Card, CardContent, Typography, Box, Button } from '@mui/material';
 import { Palette, MusicNote, Science, Favorite, Star, MenuBook } from '@mui/icons-material';
-import './ActivityLab.css';
+import './activityLab.css';
 import activity from "../../assets/picsM/activitylab.webp"
 
 const activities = [
